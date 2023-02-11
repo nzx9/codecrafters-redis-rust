4ea888df4ff7d7cffadec21e9453edbc1f8d7527
Navindu Dananga <navindudananga123@gmail.com>
2023-02-11 15:41:42 +0530

    PONG response for ping configured

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 // Uncomment this block to pass the first stage
+use std::io::{Read, Write};
 use std::net::TcpListener;
-
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    
+
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let mut buf = [0; 512];
+                _stream.read(&mut buf).unwrap();
+                println!("received: {}", String::from_utf8_lossy(&buf));
+                _stream.write("+PONG\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
